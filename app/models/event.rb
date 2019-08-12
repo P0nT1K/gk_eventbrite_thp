760fbe8ed97c40b2c95262734816e5ceac746015
P0nt1K <gregoire-kelesoglu@hotmail.fr>
2019-08-12 14:27:36 +0200

    1er ajout

---
+++ app/models/event.rb
@@ -0,0 +1,41 @@
+class Event < ApplicationRecord
+    validates :start_date, presence: true, if: :is_past?
+
+    validates :duration, presence: true,  numericality: {only_integer: true, message: "Muste be an integer", minimum: 0}, if: :is_modulo5?
+    # le nombre de minutes doit être un multiple de 5, et est strictement positif
+
+    validates :title, 
+    presence: true,
+    length: { in: 5..140 }
+    #doit faire au moins 5 caractères et maxi 140 caractères.
+
+    validates :description, 
+    presence: true,
+    length: { in: 20..1000 }
+    #doit faire entre 20 et 1000 caractères.
+   
+    validates :price, 
+    presence: true,
+    numericality: {only_integer: true, message: "Muste be an integer", in: 1..1000}
+    #doit être compris entre 1 et 1000.
+
+    validates :location, presence: true
+
+
+    belongs_to :administrator, class_name: "User" 
+    #appartiens à un User qui est administrateur
+    has_many :attendances
+    #un event à pluieurs inscription(attendences)
+    has_many :users, through: :attendances
+    #un event à plusieurs participants(user) via les insecriptions(attenances)
+
+    def is_past?
+        start_date >= Times.now
+    end
+
+    def is_modulo5?
+        (duration/5).class == Integer     
+    end
+end
+
+
---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+    
+    has_many :attendances
+    #un user à plusieurs inscriptions(attendences)
+    has_many :events, through: :attendances
+    #participe à plusieurs EVT via les inscriptions
+    has_many :event_admin, foreign_key: 'administrator_id', class_name 'Event'
+    #event_admin correspond à une méthode qui permet de recuperer l'index de l'administrator
+    #l'adiminstrator_id corespond au User_id
+    #User.find(2).event_admin permet d'afficher la liste de tous les events crées par le User(2)
+end
